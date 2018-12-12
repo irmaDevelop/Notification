713212180b4abe96f8436b18ae367ffaf8400f16
IrmaJulia <irmajuliadavila@icloud.com>
2018-12-12 09:27:05 -0500

    fist commit

---
+++ app/src/main/java/com/irmadevelop/authentication/PlatziFirebaseInstanceIdService.java
@@ -0,0 +1,8 @@
+package com.irmadevelop.authentication;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PlatziFirebaseInstanceIdService extends FirebaseMessagingService {
+
+
+}
